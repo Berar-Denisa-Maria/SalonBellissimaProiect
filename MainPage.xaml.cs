e0ef7620d45dd0a92147dbddd621e4d6504ee19e
Berar-Denisa-Maria <denisa.berar@stud.ubbcluj.ro>
2025-01-08 00:08:44 +0200

    merge dar mai trebuie modificate unele lucruri

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace SalonBellissima
+﻿using SalonBellissima.Models;
+namespace SalonBellissima
 {
     public partial class MainPage : ContentPage
     {
@@ -6,6 +7,7 @@
         public MainPage()
         {
             InitializeComponent();
+            VerificaProgramari();
         }
 
         private async void OnCategoriiButtonClicked(object sender, EventArgs e)
@@ -13,7 +15,33 @@
             await Navigation.PushAsync(new ListPage());
         }
 
+        private async void VerificaProgramari()
+        {
+            // Se incarca programarile existente
+            var programari = await App.Database.GetProgramariAsync();
+            DateTime dataMaine = DateTime.Now.Date.AddDays(1);
 
+            // Se cauta programarea pentru ziua urmatoare
+            var programareMaine = programari.FirstOrDefault(p => p.DataOra.Date == dataMaine);
+
+            // Daca exista, se afiseaza notificarea
+            if (programareMaine != null)
+            {
+                var serviciu = await App.Database.GetServiciuAsync();
+                var serviciuSelectat = serviciu.FirstOrDefault(s => s.Id == programareMaine.ServiciuId);
+                notificareLabel.Text = $"Maine aveti programarea: {serviciuSelectat.DenumireServiciu} la ora {programareMaine.DataOra.ToShortTimeString()}";
+            }
+            else
+            {
+                notificareLabel.Text = "Nu aveti programari pentru maine.";
+            }
+        }
+
+        private async void OnNavigareProgramariClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new PaginaProgramare());
+        }
     }
 
 }
+
